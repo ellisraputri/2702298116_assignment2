80ca5cf6e12eca7cc786cb853ecc3826872a6e82
ellisraputri <ellisraputri@gmail.com>
2025-02-18 12:22:22 +0700

    update

--- indexscript.js
+++ indexscript.js
@@ -1,16 +1,7 @@
+$(document).ready(function(){
+  $("#nmc-button").click(function(){
+      $("#content-hide").slideToggle(300); 
+  });
+});
 
 
-function nmc_button() {
-    var moreText = document.getElementById("content-hide");
-    var btnText = document.getElementById("nmc-button");
-
-    var currentDisplay = window.getComputedStyle(moreText).display;
-  
-    if (currentDisplay === "none") {
-      btnText.innerHTML = "Hide details";
-      moreText.style.display = "inline";
-    } else {
-      btnText.innerHTML = "Click here for details!";
-      moreText.style.display = "none";
-    }
-}
